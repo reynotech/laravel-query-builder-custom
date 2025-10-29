8b46518b3ec8aae8be43e26c6aff7bd9ad4f7cb5
Samuel Casas <samuel@reyno.tech>
2025-10-29 12:01:28 -0500

    advanced

--- src/Filters/StringAdvancedFilter.php
+++ src/Filters/StringAdvancedFilter.php
@@ -12,53 +12,56 @@ class StringAdvancedFilter implements Filter
 
     protected ?Closure $closuredQuery = null;
 
-    protected array $filters = [
-        'eq' => [
-            'op' => '='
-        ],
-        'neq' => [
-            'op' => '<>'
-        ],
-        'con' => [
-            'op' => 'LIKE',
-            'string' => "%?%"
-        ],
-        'ncon' => [
-            'op' => 'NOT LIKE',
-            'string' => "%?%"
-        ],
-        'e' => [
-            'string' => ':col: IS NULL OR :col: = \'\'',
-            'rawString' => true
-        ],
-        'ne' => [
-            'string' => ':col: IS NOT NULL OR :col: <> \'\'',
-            'rawString' => true
-        ],
-        'bw' => [
-            'op' => 'LIKE',
-            'string' => "?%"
-        ],
-        'ew' => [
-            'op' => 'LIKE',
-            'string' => "%?"
-        ],
-        'nbw' => [
-            'op' => 'NOT LIKE',
-            'string' => "?%"
-        ],
-        'new' => [
-            'op' => 'NOT LIKE',
-            'string' => "%?"
-        ],
-        'in' => [
-            'op' => 'in'
-        ]
-    ];
-
     public function __construct(Closure $query = null)
     {
         $this->closuredQuery = $query;
+    }
+
+    public function getFilters()
+    {
+        $likeFn = fn($cond = 'LIKE', $par = null) => fn($query, $value, $property) => str_contains($property, '->') ?
+            $query->whereRaw(
+                "CAST({$query->getGrammar()->wrap($property)} as CHAR) {$cond} ?",
+                ['%' . $value . '%']
+            ) : $query->where($property, $cond, $par ? $par($value) : '%' . $value . '%');
+
+        return [
+            'eq' => [
+                'op' => '='
+            ],
+            'neq' => [
+                'op' => '<>'
+            ],
+            'con' => [
+                'query' => fn($query, $value, $property) => $likeFn()($query, $value, $property)
+            ],
+            'ncon' => [
+                'query' => fn($query, $value, $property) => $likeFn('NOT LIKE')($query, $value, $property)
+            ],
+            'e' => [
+                'string' => ':col: IS NULL OR :col: = \'\'',
+                'rawString' => true
+            ],
+            'ne' => [
+                'string' => ':col: IS NOT NULL OR :col: <> \'\'',
+                'rawString' => true
+            ],
+            'bw' => [
+                'query' => fn($query, $value, $property) => $likeFn(par: fn($v) => $v . '%')($query, $value, $property)
+            ],
+            'ew' => [
+                'query' => fn($query, $value, $property) => $likeFn(par: fn($v) => '%' . $v)($query, $value, $property)
+            ],
+            'nbw' => [
+                'query' => fn($query, $value, $property) => $likeFn('NOT LIKE', fn($v) => $v . '%')($query, $value, $property)
+            ],
+            'new' => [
+                'query' => fn($query, $value, $property) => $likeFn('NOT LIKE', fn($v) => '%' . $v)($query, $value, $property)
+            ],
+            'in' => [
+                'op' => 'in'
+            ]
+        ];
     }
 
     public function __invoke(Builder $query, $value, string $property): void
@@ -70,37 +73,39 @@ class StringAdvancedFilter implements Filter
         }
     }
 
-    public function processQuery($query, $value, $property): void
+    public function processQuery($query, $value, $property)
     {
+        $filters = $this->getFilters();
         list($operation, $value) = $value;
 
-        if($this->filters[$operation]) {
-            $operation = $this->filters[$operation];
+        if($filters[$operation]) {
+            $operation = $filters[$operation];
 
-            if (isset($operation['rawString'])) {
-                $ocurrences = [
-                    ':col:' => $property
-                ];
-                $query->where(DB::raw(strtr($operation['string'], $ocurrences)));
-            }
-            else if ($operation['op'] === 'in') {
-                $query->whereIn($property, explode(',', $value));
-            }
-            else
-            {
-                if(isset($operation['raw'])) {
-                    $op = DB::raw($operation['op']);
+            if (array_key_exists('query', $operation)) {
+                $operation['query']($query, $value, $property);
+            } else {
+                if (isset($operation['rawString'])) {
+                    $occurrences = [
+                        ':col:' => $property,
+                    ];
+                    $query->where(DB::raw(strtr($operation['string'], $occurrences)));
+                } else if ($operation['op'] === 'in') {
+                    $query->whereIn($property, explode(',', $value));
                 } else {
-                    $op = $operation['op'];
-                }
+                    if (isset($operation['raw'])) {
+                        $op = DB::raw($operation['op']);
+                    } else {
+                        $op = $operation['op'];
+                    }
 
-                if(isset($operation['string'])) {
-                    $val = Str::replaceArray('?', [$value], $operation['string']);
-                } else {
-                    $val = $value;
-                }
+                    if (isset($operation['string'])) {
+                        $val = Str::replaceArray('?', [$value], $operation['string']);
+                    } else {
+                        $val = $value;
+                    }
 
-                $query->where($property, $op, $val);
+                    $query->where($property, $op, $val);
+                }
             }
         }
     }
